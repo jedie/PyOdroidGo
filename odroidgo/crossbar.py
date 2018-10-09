87fd6d26ec53c8fdbc3355f4cfb1b3b36bc68c42
JensDiemer <git@jensdiemer.de>
2018-10-09 19:50:18 +0200

    bugfix crossbar

--- odroidgo/crossbar.py
+++ odroidgo/crossbar.py
@@ -1,24 +1,38 @@
 """
-    Odroid Go crossbar handler
+    ODROID GO Crossbar
 
-    joy-x button adc values: left 511 - right 234
-    joy-y button adc values: up 511 - down 234
+    release value = 142
+
+    joy-x button adc values: left 2500 - right 1200
+    joy-y button adc values: up 2500 - down 1200
 """
+import sys
+import time
 
-
-from machine import ADC, Pin, Timer
-
+import machine
 import odroidgo
+from machine import ADC, Pin, Timer
 
 
 class JoystickHandler:
     def __init__(self, pin_no, high_callback, low_callback):
-        pin = Pin(pin_no, mode=Pin.IN, pull=Pin.PULL_UP)
+        # set PULL_UP will raise into:
+        # ValueError: pins 34~39 do not have pull-up or pull-down circuitry
+        pin = Pin(pin_no, mode=Pin.IN)  # , pull=Pin.PULL_UP)
 
         self.high_callback = high_callback
         self.low_callback = low_callback
 
-        self.adc = ADC(pin)
+        try:
+            self.adc = ADC(pin)
+        except OSError as err:
+            # e.g.: ValueError: pin already used for adc
+            sys.print_exception(err)
+            for i in range(3, 0, -1):
+                print("Hard reset in %i Sek!" % i)
+                time.sleep(1)
+            machine.reset()
+
         self.adc.width(ADC.WIDTH_9BIT)
         self.adc.atten(ADC.ATTN_11DB)
 
