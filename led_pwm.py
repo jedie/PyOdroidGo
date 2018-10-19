914afb4e395f0aa579c40e379b6147c18be89721
JensDiemer <git@jensdiemer.de>
2018-10-19 22:46:13 +0200

    update led_pwm.py

--- led_pwm.py
+++ led_pwm.py
@@ -1,23 +1,31 @@
-from machine import Pin, PWM
-from micropython import const
-
 import time
 
-def main(lcd, print_func):
-    BLUE_LED = const(2)
-    blue_led = Pin(BLUE_LED, Pin.OUT)
+from machine import PWM, Pin
+from odroidgo.led import blue_led
+
+
+def main(screen):
     led_pwm = PWM(blue_led, freq=1000, duty=0)
 
     for i in range(3, 0, -1):
-        print_func("%i" % i)
+        screen.print("%i" % i)
 
-        for value in range(0, 1023, 100):
+        for value in range(0, 100, 10):
             led_pwm.duty(value)
-            time.sleep(0.05)
+            time.sleep(0.01)
 
-        for value in range(1023, 0, -100):
+        for value in range(100, 0, -10):
             led_pwm.duty(value)
-            time.sleep(0.05)
+            time.sleep(0.01)
 
     led_pwm.deinit()
 
+
+if __name__ == "builtins":
+    from odroidgo.screen import OdroidGoDisplay
+
+    screen = OdroidGoDisplay()
+    main(screen)
+    screen.deinit()
+    del screen
+    print("---END---")
