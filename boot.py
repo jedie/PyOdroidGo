4c5f1e3195bf248d749374ded3e77546af5f60a7
JensDiemer <git@jensdiemer.de>
2018-10-19 20:27:35 +0200

    faster boot

--- boot.py
+++ boot.py
@@ -2,42 +2,12 @@
 
 print(" *** boot.py *** ")
 
-
-def print_time():
+def main():
     import time
-
     print("time:", time.time())
 
     from machine import RTC
-
     print("RTC datetime:", RTC().now())
-
-
-def led_pwm():
-    import time
-    import odroidgo
-    from machine import Pin, PWM
-
-    blue_led = Pin(odroidgo.LED_BLUE, Pin.OUT)
-    led_pwm = PWM(blue_led, freq=1000, duty=0)
-
-    for i in range(3, 0, -1):
-        print("Boot wait %i..." % i)
-
-        for value in range(0, 100, 10):
-            led_pwm.duty(value)
-            time.sleep(0.01)
-
-        for value in range(100, 0, -10):
-            led_pwm.duty(value)
-            time.sleep(0.01)
-
-    led_pwm.deinit()
-
-
-def main():
-    print_time()
-    led_pwm()
 
 
 main()
